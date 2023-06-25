40578233bca084ecf9cefea0ef7b54cc0740a20f
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-06-25 19:10:35 +0300

    add collision detection

--- axe-game/axe_game.cpp
+++ axe-game/axe_game.cpp
@@ -10,10 +10,20 @@ int main()
   int circle_x{200};
   int circle_y{200};
   int circle_r{25};
-
+  // circle edges
+  int l_circle_x{circle_x - circle_r};
+  int r_circle_x{circle_x + circle_r};
+  int u_circle_y{circle_y - circle_r};
+  int b_circle_y{circle_y + circle_r};
   // axe coordinates
   int axe_x{400};
   int axe_y{0};
+  int axe_length{50};
+  // axe edges
+  int l_axe_x{axe_x};
+  int r_axe_x{axe_x + axe_length};
+  int u_axe_y{axe_y};
+  int b_axe_y{axe_y + axe_length};
 
   int direction{10};
 
@@ -29,20 +39,20 @@ int main()
     // int width{800};
     // int height{450};
     DrawCircle(circle_x, circle_y, circle_r, BLUE);
-    DrawRectangle(axe_x, axe_y, 50, 50, RED);
+    DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
 
     // move the axe
     axe_y += direction;
-    if (axe_y > 400 || axe_y < 0)
+    if (axe_y > height || axe_y < 0)
     {
       direction = -direction;
     }
 
-    if (IsKeyDown(KEY_D) && circle_x < 775)
+    if (IsKeyDown(KEY_D) && circle_x < width)
     {
       circle_x = circle_x + 10;
     }
-    if (IsKeyDown(KEY_A) && circle_x > 25)
+    if (IsKeyDown(KEY_A) && circle_x > 0)
     {
       circle_x -= 10;
     }
