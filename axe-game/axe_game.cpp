7493e8e9607ed946cbe2a83ef3c6214ee8dc0ad9
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-06-25 18:44:05 +0300

    draw the axe

--- axe-game/axe_game.cpp
+++ axe-game/axe_game.cpp
@@ -3,13 +3,17 @@
 int main()
 {
   // window dimensions
-  int width{350};
-  int height{250};
+  int width{800};
+  int height{450};
   InitWindow(width, height, "Anil's Window");
   // circle coordinates
-  int circle_x{175};
-  int circle_y{100};
+  int circle_x{200};
+  int circle_y{200};
   int circle_r{25};
+
+  // axe coordinates
+  int axe_x{400};
+  int axe_y{0};
 
   SetTargetFPS(60);
   // window popup constantly open with this while loop
@@ -20,9 +24,15 @@ int main()
 
     // Game logic begins
 
+    // int width{800};
+    // int height{450};
     DrawCircle(circle_x, circle_y, circle_r, BLUE);
+    DrawRectangle(axe_x, axe_y, 50, 50, RED);
 
-    if (IsKeyDown(KEY_D) && circle_x < 325)
+    // move the axe
+    axe_y += 10;
+
+    if (IsKeyDown(KEY_D) && circle_x < 775)
     {
       circle_x = circle_x + 10;
     }
