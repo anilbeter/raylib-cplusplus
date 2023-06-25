02516d5dbbf608f5e20d5f41abddd25a9a0ecb43
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-06-25 14:14:35 +0300

    add while loop

--- axe-game/axe_game.cpp
+++ axe-game/axe_game.cpp
@@ -2,16 +2,11 @@
 
 int main()
 {
-  float root_beer;
-  root_beer = 1.99f;
-  double cheese_burger{5.99};
-  bool shouldHaveLunch{};
-  bool equal{4 == 9};        // false
-  bool notEqual{4 != 9};     // true
-  bool less{4 < 9};          // true
-  bool lessEqual{4 <= 9};    // true
-  bool greaterEqual{4 >= 9}; // false
   int width = 350;
   int height = 200;
   InitWindow(width, height, "Anil's Window");
+  // window popup constantly open with this while loop
+  while (true)
+  {
+  }
 }
