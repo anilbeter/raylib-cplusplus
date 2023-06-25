a2e7eba36e08447115829ca8ce320ebb0fd5e1fc
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-06-25 18:28:48 +0300

    moving the circle

--- axe-game/axe_game.cpp
+++ axe-game/axe_game.cpp
@@ -2,16 +2,36 @@
 
 int main()
 {
-  int width = 350;
-  int height = 200;
+  // window dimensions
+  int width{350};
+  int height{250};
   InitWindow(width, height, "Anil's Window");
-  // window popup constantly open with this while loop
+  // circle coordinates
+  int circle_x{175};
+  int circle_y{100};
+  int circle_r{25};
 
+  SetTargetFPS(60);
+  // window popup constantly open with this while loop
   while (!WindowShouldClose())
   {
     BeginDrawing();
     ClearBackground(WHITE);
-    DrawCircle(175, 100, 25, BLUE);
+
+    // Game logic begins
+
+    DrawCircle(circle_x, circle_y, circle_r, BLUE);
+
+    if (IsKeyDown(KEY_D))
+    {
+      circle_x = circle_x + 10;
+    }
+    if (IsKeyDown(KEY_A))
+    {
+      circle_x -= 10;
+    }
+
+    // Game logic ends
     EndDrawing();
   }
 }
