5cffdd376e1bea4549c2cf50ab93a799533b22dc
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-06-25 18:12:57 +0300

    add WindowShouldClose() function

--- axe-game/axe_game.cpp
+++ axe-game/axe_game.cpp
@@ -6,7 +6,8 @@ int main()
   int height = 200;
   InitWindow(width, height, "Anil's Window");
   // window popup constantly open with this while loop
-  while (true)
+
+  while (!WindowShouldClose())
   {
     BeginDrawing();
     ClearBackground(RED);
