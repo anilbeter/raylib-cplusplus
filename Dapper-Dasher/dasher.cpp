a902ecc515bfe3d5cbcd64ce5a6e98c365c8a80b
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-07-09 18:31:02 +0300

    add velocity and jumping stuff

--- Dapper-Dasher/dasher.cpp
+++ Dapper-Dasher/dasher.cpp
@@ -1,4 +1,4 @@
-#include <raylib.h>
+#include "raylib.h"
 
 int main()
 {
@@ -7,11 +7,26 @@ int main()
   SetTargetFPS(60);
   InitWindow(windowWidth, windowHeight, "BURNING UP!");
 
+  // rectangle dimensions
+  const int width{50};
+  const int height{80};
+
+  int posY{windowHeight - height};
+  int velocity{0};
+
   while (!WindowShouldClose())
   {
     // Start Drawing
     BeginDrawing();
-    ClearBackground(RED);
+    ClearBackground(WHITE);
+
+    if (IsKeyPressed(KEY_SPACE))
+    {
+      velocity -= 10;
+    }
+    posY += velocity;
+
+    DrawRectangle(windowWidth / 2, posY, width, height, BLUE);
 
     // Stop drawing
     EndDrawing();
