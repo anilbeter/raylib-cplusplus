4ccfe703abfac540e55a8d53ec543c0fbafe910c
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-07-28 18:51:29 +0300

    add gravity and acceleration

--- v2/Dapper Dasher/dasher.cpp
+++ v2/Dapper Dasher/dasher.cpp
@@ -8,6 +8,9 @@ int main()
 
   InitWindow(windowWidth, windowHeight, "Dapper Dasher");
 
+  // acceleration due to gravity (pixels/frame)/frame
+  const int gravity = 1;
+
   // Rectangle dimensions
   const int width = 50;
   const int height = 80;
@@ -21,11 +24,26 @@ int main()
     BeginDrawing();
     ClearBackground(WHITE);
 
+    // perform ground check
+    if (posY >= windowHeight - height)
+    {
+      // rectangle is on ground
+      velocity = 0;
+    }
+    else
+    {
+      // rectangle is in the air
+      // apply gravity
+      velocity += gravity;
+    }
+
     DrawRectangle(windowWidth / 2, posY, width, height, RED);
     if (IsKeyPressed(KEY_SPACE))
     {
       velocity -= 10;
     }
+
+    // update position
     posY += velocity;
 
     EndDrawing();
