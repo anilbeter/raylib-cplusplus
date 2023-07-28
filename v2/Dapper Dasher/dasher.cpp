6255dea6462656161c7db92c9ba2979c000be379
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-07-28 18:20:40 +0300

    add velocity and jumping

--- v2/Dapper Dasher/dasher.cpp
+++ v2/Dapper Dasher/dasher.cpp
@@ -6,12 +6,28 @@ int main()
   const int windowWidth = 512;
   const int windowHeight = 380;
 
-  SetTargetFPS(60);
   InitWindow(windowWidth, windowHeight, "Dapper Dasher");
+
+  // Rectangle dimensions
+  const int width = 50;
+  const int height = 80;
+
+  int posY = windowHeight - height;
+  int velocity = 0;
+
+  SetTargetFPS(60);
   while (!WindowShouldClose())
   {
     BeginDrawing();
     ClearBackground(WHITE);
+
+    DrawRectangle(windowWidth / 2, posY, width, height, RED);
+    if (IsKeyPressed(KEY_SPACE))
+    {
+      velocity -= 10;
+    }
+    posY += velocity;
+
     EndDrawing();
   }
   CloseWindow();
