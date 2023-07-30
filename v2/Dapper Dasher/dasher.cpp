74483d716f97590b358455a9b579879556829849
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-07-30 09:49:01 +0300

    replace array variables

--- v2/Dapper Dasher/dasher.cpp
+++ v2/Dapper Dasher/dasher.cpp
@@ -15,8 +15,8 @@ int main()
   windowDimensions[0] = 512;
   windowDimensions[1] = 380;
 
-  const int windowWidth = 512;
-  const int windowHeight = 380;
+  // const int windowWidth = 512;
+  // const int windowHeight = 380;
 
   InitWindow(windowDimensions[0], windowDimensions[1], "Dapper Dasher");
 
@@ -28,16 +28,16 @@ int main()
 
   // AnimData for nebula
   AnimData nebData{
-      {0.0, 0.0, nebula.width / 8, nebula.height / 8}, // Rectangle rec
-      {windowWidth, windowHeight - nebula.height / 8}, // Vector2 pos
-      0,                                               // int frame
-      1.0 / 12.0,                                      // float updateTime
-      0                                                // float runningTime
+      {0.0, 0.0, nebula.width / 8, nebula.height / 8},                // Rectangle rec
+      {windowDimensions[0], windowDimensions[1] - nebula.height / 8}, // Vector2 pos
+      0,                                                              // int frame
+      1.0 / 12.0,                                                     // float updateTime
+      0                                                               // float runningTime
   };
 
   AnimData neb2Data{
       {0.0, 0.0, nebula.width / 8, nebula.height / 8},
-      {windowWidth + 300, windowHeight - nebula.height / 8},
+      {windowDimensions[0] + 300, windowDimensions[1] - nebula.height / 8},
       0,
       1.0 / 16.0,
       0.0};
@@ -64,8 +64,8 @@ int main()
   scarfyData.rec.height = scarfy.height;
   scarfyData.rec.x = 0;
   scarfyData.rec.y = 0;
-  scarfyData.pos.x = windowWidth / 2 - scarfyData.rec.width / 2;
-  scarfyData.pos.y = windowHeight - scarfyData.rec.height;
+  scarfyData.pos.x = windowDimensions[0] / 2 - scarfyData.rec.width / 2;
+  scarfyData.pos.y = windowDimensions[1] - scarfyData.rec.height;
   scarfyData.frame = 0;
   scarfyData.updateTime = 1.0 / 12.0;
   scarfyData.runningTime = 0.0;
@@ -101,7 +101,7 @@ int main()
     const float dT = GetFrameTime();
 
     // perform ground check
-    if (scarfyData.pos.y >= windowHeight - scarfyData.rec.height)
+    if (scarfyData.pos.y >= windowDimensions[1] - scarfyData.rec.height)
     {
       // rectangle is on ground
       velocity = 0;
