ef0889d20755dda3bdd0ec56c8ce8c3d3efa596c
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-01 19:13:30 +0300

    implement movement input (vectors)

--- v2/clash rpg/main.cpp
+++ v2/clash rpg/main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 
 int main()
 {
@@ -9,6 +10,8 @@ int main()
   InitWindow(windowDimensions[0], windowDimensions[1], "Dream On");
 
   Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
+  Vector2 mapPos = {0.0, 0.0};
+  float speed = 4.0;
 
   SetTargetFPS(60);
   while (!WindowShouldClose())
@@ -16,7 +19,22 @@ int main()
     BeginDrawing();
     ClearBackground(WHITE);
 
-    Vector2 mapPos = {0.0, 0.0};
+    Vector2 direction{};
+    if (IsKeyDown(KEY_A))
+      direction.x -= 1.0;
+    if (IsKeyDown(KEY_D))
+      direction.x += 1.0;
+    if (IsKeyDown(KEY_W))
+      direction.y -= 1.0;
+    if (IsKeyDown(KEY_S))
+      direction.y += 1.0;
+    if (Vector2Length(direction) != 0.0)
+    {
+      // set mapPos = mapPos - direction
+
+      mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+    }
+
     DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
     EndDrawing();
